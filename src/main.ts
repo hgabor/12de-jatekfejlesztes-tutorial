2bfe64d8e415cd273ea067f766b3b579f4c00f69
Petrik Diak <diak@petrik.hu>
2025-03-28 11:29:21 +0100

    Tutorial assets, basic screen

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,17 @@ import { AUTO, Game, Scale,Types } from 'phaser';
 //  https://newdocs.phaser.io/docs/3.70.0/Phaser.Types.Core.GameConfig
 const config: Types.Core.GameConfig = {
     type: AUTO,
-    width: 1024,
-    height: 768,
+    width: 800,
+    height: 600,
     parent: 'game-container',
     backgroundColor: '#028af8',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300, x: 0 },
+            debug: false
+        }
+    },
     scale: {
         mode: Scale.FIT,
         autoCenter: Scale.CENTER_BOTH
--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -10,21 +10,41 @@ export class Game extends Scene
     preload ()
     {
         this.load.setPath('assets');
-        
-        this.load.image('background', 'bg.png');
-        this.load.image('logo', 'logo.png');
+        this.load.image('sky', 'sky.png');
+        this.load.image('platform', 'platform.png');
+        this.load.image('star', 'star.png');
+        this.load.image('bomb', 'bomb.png');
+        this.load.spritesheet('dude', 'dude.png', {
+            frameWidth: 32,
+            frameHeight: 48,
+        });
     }
 
     create ()
     {
-        
-        this.add.image(512, 384, 'background');
-        this.add.image(512, 350, 'logo').setDepth(100);
-        this.add.text(512, 490, 'Make something fun!\nand share it with us:\nsupport@phaser.io', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        }).setOrigin(0.5).setDepth(100);
+        this.add.image(0, 0, 'sky').setOrigin(0, 0);
+
+        let platforms = this.physics.add.staticGroup();
+        platforms.create(400, 568, 'platform').setScale(2).refreshBody();
+
+        platforms.create(600, 400, 'platform');
+        platforms.create(50, 250, 'platform');
+        platforms.create(750, 220, 'platform');
+
+        let player = this.physics.add.sprite(100, 450, 'dude');
+
+        player.setBounce(0.2);
+        player.setCollideWorldBounds(true);
+
+
+        /*
+        for (let i = -4; i < 5; i++) {
+            this.add.image(400 + (i * 30) , 300, 'star');
+        }
+            */
+    }
+
+    update(): void {
         
     }
 }
